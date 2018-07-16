b78fe9e1e202ff924d6263502469e06c0f794a1a
be-bert <bebert.apps@gmail.com>
2018-07-16 15:24:25 -0400

    Attempt to hit the API and return a list of people. This will fail since there is no real endpoint.

---
+++ Swifter/PersonRepository.swift
@@ -0,0 +1,38 @@
+
+import Foundation
+
+class PersonRepository {
+    
+    let session = URLSession(configuration: .default)
+    private var dataTask: URLSessionDataTask?
+    
+    func getPeople(completion: @escaping ([Person]?, Error?) -> Void) {
+        
+        dataTask?.cancel()
+        
+        let url = URL(fileURLWithPath: URLSettings().peopleURL)
+        
+        dataTask = session.dataTask(with: url) { (data, response, error) in
+            defer { self.dataTask = nil }
+            
+            DispatchQueue.main.async {
+                if let data = data {
+                    
+                    let decoder = JSONDecoder()
+                    do {
+                        let people = try decoder.decode([Person].self, from: data)
+                        completion(people, nil)
+                    } catch {
+                        print("Error decoding person: \(error)")
+                        completion(nil, error)
+                    }
+                    
+                } else if let error = error {
+                    completion(nil, error)
+                }
+            }
+        }
+        dataTask?.resume()
+    }
+    
+}
