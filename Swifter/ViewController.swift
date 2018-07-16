84aabad28f3c9ae30e532f35681c187f9feb9270
be-bert <bebert.apps@gmail.com>
2018-07-16 11:18:59 -0400

    Default TableView setup.
    Added a Person Cell that will display the Person Model info.

--- Swifter/ViewController.swift
+++ Swifter/ViewController.swift
@@ -12,14 +12,28 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
 
 }
 
+extension ViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let personCell =  tableView.dequeueReusableCell(withIdentifier: "personCell") as! PersonCell
+        
+        return personCell
+    }
+    
+}
+
+extension ViewController: UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return UITableViewAutomaticDimension
+    }
+    
+}
